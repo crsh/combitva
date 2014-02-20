4125e69da75ef259432e743ea35aefff2d3a2a45
Frederik Aust <frederik@frederikaust.(none)>
2014-02-20 11:34:18 +0100

    Added instructions to readme and changed variable names in libTVA-function for better readability.

--- README.md
+++ README.md
@@ -1,17 +1,19 @@
-# CombiTVA
-This is an implementation of CombiTVA paradigm (Vangkilde, Bundesen & Coull, 2011) in OpenSesame (Mathôt, Schreij & Theeuwes, 2012).
+# Open CombiTVA
+This is an open implementation of the CombiTVA paradigm (Vangkilde, Bundesen & Coull, 2011) in OpenSesame (Mathôt, Schreij & Theeuwes, 2012).
+
+This version of Open CombiTVA is out of the box optimized for 19-inch displays with a resolution of 1280 x 960 px at 100 Hz refresh rate for proper stimulus presentation. Participant should be seated at a distance of 60 cm from the display during the experiment. Of course the experiment can be customized to fit other screen sizes and viewing distances.
 
 In the folder *analysis* you'll find an R-Script that converts the data to the format needed for parameter fitting. The converted files can be fit using either the trial-by-trial fitting program (Kyllingsbæk, 2006) or the LibTVA MatLab library (Dyrholm, Kyllingsbæk, Espeseth & Bundesen, 2011). You can retrieve both software packages from [Claus Bundesen's lab website](http://cvc.psy.ku.dk/resources/).
 
 More resources on OpenSesame can be found at on [Sebastiaan Mathôt's website](http://osdoc.cogsci.nl/).
 
-# Contributions
+## Contributions
 If you find a bug or have ideas on how to improve the implementation of the experiment you are more than welcome to open an issue or, even better, fork the project and provide solutions in a pull request. :)
 
-# Citing
-If you'd like to add a reference in your work to make researchers aware of this open implementation of CombiTVA, you may (for now) use the following reference:
+## Citing Open CombiTVA
+If you'd like to cite Open CombiTVA in your work, you may use the following reference:
 
-Aust, F. & Papenberg, M. (2014). An open implementation of the CombiTVA paradigm in OpenSesame. *github.com*. Retrieved from https://github.com/crsh/combitva
+Papenberg, M. & Aust, F. (2014). An open implementation of the CombiTVA paradigm in OpenSesame. *github.com*. Retrieved from https://github.com/crsh/combitva
 
 # References
 Dyrholm, M., Kyllingsbæk, S., Espeseth, T. & Bundesen, C. (2011). Generalizing parametric models by introducing trial-by-trial parameter variability: The case of TVA. *Journal of Mathematical Psychology*, 55, 416-429. doi: [10.1016/j.jmp.2011.08.005](http://dx.doi.org/10.1016/j.jmp.2011.08.005)
--- analysis/libTVA.r
+++ analysis/libTVA.r
@@ -3,23 +3,23 @@
 libTVA <- function(x, filename, filepath) {
   # Write trial number into the output file
   output.name <- paste0(filepath, filename, ".dat")
-  expblockdata <- subset(x, Block != 0) # Exclude practice trials
-  write.table(length(expblockdata$Block), file = output.name, quote = FALSE, col.names = FALSE, row.names = FALSE, sep = "\t", append = TRUE)
+  exp_blocks <- subset(x, Block != 0) # Exclude practice trials
+  write.table(length(exp_blocks$Block), file = output.name, quote = FALSE, col.names = FALSE, row.names = FALSE, sep = "\t", append = TRUE)
   
   # Whole report data
   for(cond in c(1:3)) { # 1 = whole report 6; 2 = whole report 2; 3 = partial report (2 targets, 4 distractors)
-    whole_rep <- subset(expblockdata, expblockdata$Condition == cond)
-    whole_rep$Letter_duration <- whole_rep$Letter_duration + 5 # Correct to represent actual presentation times
-    whole_rep[, paste0("L", 1:6)] <- apply(whole_rep[, paste0("L", 1:6)], 2, as.character)
+    tva_data <- subset(exp_blocks, exp_blocks$Condition == cond)
+    tva_data$Letter_duration <- tva_data$Letter_duration + 5 # Correct to represent actual presentation times
+    tva_data[, paste0("L", 1:6)] <- apply(tva_data[, paste0("L", 1:6)], 2, as.character)
     
     ## Reformat responses (this should be fixed in OpenSesame!)
-    whole_rep$response <- toupper(whole_rep$response) # Convert to upper case
-    whole_rep$response <- gsub(" ", "", whole_rep$response)
-    whole_rep$response[whole_rep$response == "NONE" | whole_rep$response == ""] <- "-" # Code missing as "-"
+    tva_data$response <- toupper(tva_data$response) # Convert to upper case
+    tva_data$response <- gsub(" ", "", tva_data$response)
+    tva_data$response[tva_data$response == "NONE" | tva_data$response == ""] <- "-" # Code missing as "-"
     
     ## Sort targets by position (1-3 top left - bot left, 4-6 top right - bot right)
-    positions <- whole_rep[, paste0("pLetter", c(1:6))]
-    letters <- whole_rep[, paste0("L", c(1:6))]
+    positions <- tva_data[, paste0("pLetter", c(1:6))]
+    letters <- tva_data[, paste0("L", c(1:6))]
     
     if(cond %in% c(2,3)) letters[, paste0("L", c(3:6))] <- 0 # This should be fixed in OpenSesame!
     
@@ -33,10 +33,10 @@ libTVA <- function(x, filename, filepath) {
     targets <- paste0(t1, t2, t3, t4, t5, t6)
     
     if(cond %in% c(1:2)) {
-      distractors <- rep("000000", length(whole_rep$Block)) # No distractors in whole report condition
+      distractors <- rep("000000", length(tva_data$Block)) # No distractors in whole report condition
     } else {
       ## Sort distractors by position (1-3 top left - bot left, 4-6 top right - bot right)
-      letters <- whole_rep[, paste0("L", c(1:6))]
+      letters <- tva_data[, paste0("L", c(1:6))]
       letters[, paste0("L", c(1:2))] <- 0 # This should be fixed in OpenSesame!
       
       d1 <- t(letters)[t(positions == "top left")]
@@ -51,11 +51,11 @@ libTVA <- function(x, filename, filepath) {
     
     ## Combine variables that are needed for the TVA parameter estimate
     whole_data <- cbind(
-      condition = whole_rep$Condition
-      , letter_duration = whole_rep$Letter_duration
+      condition = tva_data$Condition
+      , letter_duration = tva_data$Letter_duration
       , targets
       , distractors
-      , response = whole_rep$response
+      , response = tva_data$response
     )
     
     ## Write whole report data to file
